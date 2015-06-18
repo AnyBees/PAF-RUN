edc85519fe3622d4e04a82a3b46aacd6ec2fddf1
Aurélien Balteaux <aurelien.balteaux@telecom-paristech.fr>
2015-06-18 21:52:19 +0200

    Added vars in structures.h and changes in edf-run.c

--- edf-run.c
+++ edf-run.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
 #include "structures.h"
 
 typedef struct {
@@ -10,7 +12,24 @@ typedef struct {
   pthread_t       Thread_Id;
 } Shared_T;
 
+typedef struct {
+	// sigjmp_buf context;
+	bool active; /* true when job is released till its done executing */
+			/* time in microsec */
+	unsigned long WCET; //Worst Case Execution Time
+	unsigned long period;
+	unsigned long deadline;
+	unsigned long phase;
+	unsigned long long abs_deadline;
+	unsigned long long next_release;
+	void* stack;
+} task;
+
 SD Tasks[30];
+
+timespec ts;
+timespec nextperiod;
+//timeval tp;
 
 int main (int argc, char *argv[]){
   int i;
@@ -18,6 +37,13 @@ int main (int argc, char *argv[]){
   int period;
   int totalrate;
   int nbr;
+  int firstdeadline = 32767;
+  int firstexec = NULL;
+  int nextexec = NULL;
+  int nextdeadline = 32767;
+  int timeexec = 0;
+
+  // int checktime(int )
 
   if (argc != argv[1]){
     printf("Usage : %s nombre-de-taches\n");
@@ -38,8 +64,8 @@ int main (int argc, char *argv[]){
       printf();
     }
 
-    TaskTab[1][i] = period;
-    TaskTab[2][i] = rate;
+    Tasks.periods[i] = period;
+    Tasks.rate[i] = rate;
   }
 
   for i = 0, i < TaskNbr, i++){
@@ -70,9 +96,74 @@ int main (int argc, char *argv[]){
 		printf("fin de tid %x\n", (int)Threads[i]);
 	}*/
 
+  // return 0;
+
+  while (true){
+
+    for (i = 0; i < TaskNbr; i++){
+      if (Tasks.deadlines[i] < firstdeadline && !Tasks.complete[i]){
+        firstdeadline = Tasks.deadlines[i];
+        firstexec = i;
+      }
+    }
+
+    i = 0;
+
+    while(i != firstdeadline){
+      if (Tasks.deadlines[i] < nextdeadline && !Tasks.complete[i]){
+        nextdeadline = Tasks.deadlines[i];
+        nextexec = i;
+      }
+    }
+
+    if (timeexec < Tasks.periods[nextexec]){
+      clock_gettime(CLOCK_REALTIME, &ts);
+
+      if ((nextperiod.tv_sec - ts.tv_sec) < Tasks.periods[firstexec]*Tasks.rate[firstexec]*0.01){
+        ts.tv_sec += (nextperiod.tv_sec - ts.tv_sec);
+        Tasks.completion[firstexec] = ((int)(nextperiod.tv_sec - ts.tv_sec)*100 / Tasks.periods[firstexec]*Tasks.rate[firstexec]);
+      } else {
+        ts.tv_sec += Tasks.periods[firstexec]*Tasks.rate[firstexec]*0.01;
+        Tasks.completion[firstexec] = 100;
+        Tasks.complete[firstexec] = 1;
+      }
+
+      pthread_mutex_lock(&Shared_T.Lock);
+      pthread_cond_timedwait(&Shared_T.Cond_Var, &Shared_T.Lock, &ts);
+      pthread_mutex_unlock(&Shared_T.Lock);
+
+      Tasks.deadlines[firstexec] += Tasks.periods[firstexec];
+      timexec += Tasks.periods[firstexec]*Tasks.rate[firstexec]*0.01;
+
+      /*if (Tasks.periods[nextexec] < Tasks.periods[firstexec]){
+        nextperiod.tv_sec = ts.tv_sec + (Tasks.periods[nextexec] - timeexec);
+      }*/
+    }
+
+    // if (timeexec)
+
+    // Acquittes++;
+    printf("fin du thread (0x)%x\n", (int)Zone_Part.Thread_Id);
+    /* Pour eviter que deux threads se terminent
+     * l'un  a la suite de l'autre
+     * sans que main ait pu acquitter le premier
+     */
+    // pthread_mutex_unlock(&Verrou_Acq);
+  }
+  pthread_mutex_unlock(&Shared_T.Lock);
+  /* printf("main (Tid (0x)%x) : FIN, et nombre de threads acquittes : %d, nombre de threads termines : %d\n",
+         (int)Tid_main, Acquittes,Zone_Part.Threads_Finis); */
   return 0;
 
 }
+
+/*int checktime(timespec nextperiod, pthread_cond_t Lock){
+  timespec tc;
+  while(&Lock){
+    clock_gettime(CLOCK_REALTIME, &tc);
+    if(tc.tv_sec)
+  }
+}*/
 
 /*-----------------------------------------------------------------
                             Un_Thread
--- structures.h
+++ structures.h
@@ -3,22 +3,24 @@ typedef struct DS DS;
 
 struct DS;
 
-struct PS			// Primary server 
+struct PS			// Primary server
 {
 	int name;		// name of server
 	int rate;		// rate of utilisation (percentage)
-	int size;		// number of sons to look for EDF 
+	int size;		// number of sons to look for EDF
 	DS *son[10];	        // Pointers to the sons of tree
 	DS *father;		//useful to recover the deadlines to hight level DS of the tree
 };
 
-struct DS			// dual server 
+struct DS			// dual server
 {
 	int name;		// Name of server
 	int rate;		// rate of utilisation
 	int number;		// number of task in the server
 	int periods[30];	// Tasks periods of server
 	int deadlines[30];	// Next deadline associate with each periods
+	bool complete[30]; // Indicates for each task if it was completed already (for one period)
+	int completion[30]; // Completion rate of task for given period
 	PS *son;		// The only son in the tree ( the name 0 if is a task)
 	PS *father;		// useful to recover the deadlines to hight level DS of the tree
 };
